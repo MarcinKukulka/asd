bbd0e4728e63e4af4d6ee7fd5fa4efd3bc26413c
Michal Kawczak <michal.kawczak@student.pk.edu.pl>
2023-10-01 01:15:26 +0200

    added newsletter.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ForumSection } from "@/components/forum-section";
 import { HeroSection } from "@/components/hero-section";
 import { VoteSection } from "@/components/vote-section";
+import { Newsletter } from "@/components/newsletter";
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			<HeroSection />
 			<VoteSection />
 			<ForumSection />
+			<Newsletter />
 		</main>
 	);
 }
